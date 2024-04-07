9ac4ed5765d22cffa96af2dee564ed0546e8e978
Anton Yakovlev <Kolomin58@gmail.com>
2024-04-07 12:50:37 +0300

    add click interface

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from view import CursesView
-from model import Model
-from controller import CursesMouseController
+import click
+
+from view.CursesView import CursesView
+from model.Model import Model
+from controller.CursesMouseController import CursesMouseController
 
 
-if __name__ == "__main__":
+@click.command()
+@click.option(
+    "--difficulty",
+    "-d",
+    type=click.Choice(["easy", "medium", "hard", "custom"], case_sensitive=False),
+    default="medium",
+    help="",
+)
+@click.option("--settings", "-s", nargs=3, type=(int, int, int), help="")
+def start_app(difficulty, settings):
+    print(difficulty, settings)
     model = Model()
     view = CursesView()
     controller = CursesMouseController(model, view)
     controller.main_loop()
+
+
+if __name__ == "__main__":
+    start_app()
