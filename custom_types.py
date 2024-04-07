de0170a37428bf5bcaeaa570954289794d911e73
Anton Yakovlev <Kolomin58@gmail.com>
2024-04-07 22:24:31 +0300

    add custom types

---
+++ custom_types.py
@@ -0,0 +1,6 @@
+from typing import Tuple, List
+
+
+SettingsType = Tuple[int, int, int]
+EventType = Tuple[int, int, str]
+EventTypesList = List[EventType]
---
+++ model/components.py
@@ -0,0 +1,5 @@
+class Cell:
+    pass
+
+class Board:
+    pass
