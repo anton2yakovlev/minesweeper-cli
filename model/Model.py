d311e84efb6d13f5ef14e054b7f878ac44f61844
Anton Yakovlev <Kolomin58@gmail.com>
2024-04-10 17:38:56 +0300

    add GameService class

--- model/Model.py
+++ model/Model.py
@@ -1,39 +1,13 @@
 from typing import List, Tuple
-from custom_types import EventTypesList, SettingsType
-from model.components import Game
+from custom_types import EventTypesList
+from model.components import GameService
 
 
 class Model:
     """Базовый класс для моделей."""
 
-    difficult_settings = {
-        "easy": (10, 10, 10),
-        "medium": (15, 15, 20),
-        "hard": (20, 20, 30),
-    }
-
     def __init__(self, **args):
-        self.game = self.init_game(**args)
-
-    def get_board_params(self, **args) -> SettingsType:
-        difficulty = args.get("difficulty")
-        settings = args.get("settings")
-
-        self.difficulty = difficulty
-        self.settings = settings
-        if difficulty == "custom":  # проверяем кастомные параметры игры
-            if (
-                settings[0] * settings[1] <= settings[2]
-            ):  # размер поля <= количество мин
-                raise ValueError(
-                    "Количество мин на поле не может превышать размер поля."
-                )
-            return settings
-        return self.difficult_settings[difficulty]
-
-    def init_game(self, **args) -> Game:
-        x, y, mines_num = self.get_board_params(**args)
-        return Game(x, y, mines_num)
+        self.game = GameService.init_game(**args)
 
     def open_cell(x: int, y: int) -> EventTypesList:
         pass
--- model/components.py
+++ model/components.py
@@ -1,3 +1,21 @@
+from pydantic import BaseModel, Field, validator, ValidationError
+from pydantic.types import conint
+
+
+class Settings(BaseModel):
+    x: int = Field(gt=0, le=50)
+    y: int = Field(gt=0, le=50)
+    mines: int
+
+    @validator("mines", pre=True, always=True)
+    def validate_mines(cls, value, values):
+        x = values.get("x")
+        y = values.get("y")
+        if x * y <= value:
+            raise ValueError("Количество мин на поле не может превышать размер поля.")
+        return value
+
+
 class Cell:
     pass
 
@@ -9,12 +27,39 @@ class Board:
 
 
 class Game:
-    def __init__(self, x, y, mines_num):
-        self.board = Board(x, y)
-        self.init_mines(mines_num)
+    def __init__(self, settings):
+        self.board = Board(settings.x, settings.y)
+        self.init_mines(settings.mines)
 
     def init_mines(self, mines_num: int):
         pass
 
     def get_board(self):
         return self.board
+
+
+class GameService:
+    difficult_settings = {
+        "easy": (10, 10, 10),
+        "medium": (15, 15, 20),
+        "hard": (20, 20, 30),
+    }
+
+    @classmethod
+    def make_settings_zip(cls, settings: tuple) -> dict:
+        settings_zip = zip(("x", "y", "mines"), settings)
+        return dict(settings_zip)
+
+    @classmethod
+    def get_board_params(cls, **args) -> Settings:
+        difficulty = args.get("difficulty")
+        if difficulty != "custom":
+            settings = cls.difficult_settings[difficulty]
+        else:
+            settings = args.get("settings")
+        return Settings(**cls.make_settings_zip(settings))
+
+    @classmethod
+    def init_game(cls, **args) -> Game:
+        settings = cls.get_board_params(**args)
+        return Game(settings)
