5e4a7499f5c5e1e189d350b3567a0f2cc5c13996
Anton Yakovlev <Kolomin58@gmail.com>
2024-05-18 22:17:08 +0300

    Update controller

--- controller/BaseController.py
+++ controller/BaseController.py
@@ -1,10 +1,22 @@
+from model.Model import Model
+from view.BaseView import BaseView
+
+
 class BaseController:
-    def __init__(self, model, view):
+    def __init__(self, model: Model, view: BaseView):
         self.model = model
         self.view = view
+        
 
     def handle_input(self):
-        raise NotImplementedError("Метод handle_input должен быть реализован в подклассе.")
+        raise NotImplementedError(
+            "Метод handle_input должен быть реализован в подклассе."
+        )
+    
+    def render(self):
+        raise NotImplementedError(
+            "Метод render должен быть реализован в подклассе."
+        )
 
     def main_loop(self):
         raise NotImplementedError("Метод main_loop должен быть реализован в подклассе.")
--- controller/CursesMouseController.py
+++ controller/CursesMouseController.py
@@ -3,9 +3,9 @@ import curses
 from controller.BaseController import BaseController
 
 
-class MouseController(BaseController):
+class CursesMouseController(BaseController):
     def handle_input(self, input_key):
-        if input_key == 'q':
+        if input_key == "q":
             return  # Выход из приложения
         # Дополнительная логика для обработки других вводов
 
@@ -14,14 +14,20 @@ class MouseController(BaseController):
 
     def run(self, stdscr):
         self.view.stdscr = stdscr
+        self.render()
+        curses.mousemask(1)
         while True:
-            x, y = self.view.read_event()
+            x, y = self.view.read_event(stdscr)
             if x is not None and y is not None:
                 self.handle_mouse_click(x, y)
             key = stdscr.getkey()
-            if key == 'q':
+            if key == "q":
                 break
             self.handle_input(key)
 
     def handle_mouse_click(self, x, y):
         print(f"Клик мыши в позиции: {x}, {y}")
+
+    def render(self):
+        displayed_board = self.model.get_board_status()
+        self.view.render()
