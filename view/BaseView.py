af97e1020ba034840119b4625690693e965849ba
Anton Yakovlev <Kolomin58@gmail.com>
2024-05-18 22:16:50 +0300

    Update view

--- view/BaseView.py
+++ view/BaseView.py
@@ -2,7 +2,8 @@ class BaseView:
 
     def render(self):
         raise NotImplementedError("Метод render должен быть реализован в подклассе.")
-    
-    def read_event(self):
-        raise NotImplementedError("Метод read_event должен быть реализован в подклассе.")
 
+    def read_event(self):
+        raise NotImplementedError(
+            "Метод read_event должен быть реализован в подклассе."
+        )
--- view/CursesView.py
+++ view/CursesView.py
@@ -1,22 +1,30 @@
 import curses
 
-class CursesView:
-    """Базовый класс для представлений."""
-    def __init__(self, stdscr):
-        self.stdscr = stdscr
-        # Включение поддержки событий мыши
-        curses.mousemask(1)
 
-    def render(self):
+class CursesView:
+
+    def draw_borders(self, start_x, start_y, x_lenght, y_length):
         pass
 
-    def read_mouse_event(self):
+    def render(self):
+        self.stdscr.clear()
+        #self.draw_borders(3, 3, 10, 10)
+        subwin = self.stdscr.subwin(12, 12, 2, 2)
+        subwin.box()
+        for i in range(10):
+            for j in range(10):
+                self.stdscr.addstr(i+3, j+3, "▢")
+        self.stdscr.refresh()
+        subwin.refresh()
+
+
+    def read_mouse_event(self, stdscr):
         """Считывает события мыши."""
-        event = self.stdscr.getch()
+        event = stdscr.getch()
         if event == curses.KEY_MOUSE:
             _, x, y, _, _ = curses.getmouse()
             return x, y
         return None, None
 
-    def read_event(self):
-        pass
+    def read_event(self, stdscr):
+        return self.read_mouse_event(stdscr)
